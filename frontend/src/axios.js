96818af2970b65b5fc4fff1e6af282a99f6e74a3
githubankush <ankushkumayu04@gmail.com>
2025-08-14 00:13:12 +0530

    '/api' added in axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://chatroom-0644.onrender.com',
+  baseURL: 'https://chatroom-0644.onrender.com/api',
   withCredentials: true, // Enables sending cookies with requests
   
 });
