5cd583fa1188f066831c010768b9aefecfcacb9e
githubankush <ankushkumayu04@gmail.com>
2025-08-14 18:34:12 +0530

    profile updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,7 +7,7 @@ import { useAuth } from "../context/authContext";
 
 const Profile = ({ user, onClose }) => {
   const backendBase = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
-  const { setUser } = useAuth();
+  const {user, setUser } = useAuth();
   const navigate = useNavigate();
   const [isUploading, setIsUploading] = useState(false);
 
